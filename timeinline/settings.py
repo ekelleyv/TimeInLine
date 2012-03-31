5b403ccc106d52eea1dd6075f8eeff5f34b7e952
Ed Kelley <ekelley@princeton.edu>
2012-03-31 01:22:18 -0400

    Basic API Handling

--- timeinline/settings.py
+++ timeinline/settings.py
@@ -61,7 +61,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = os.path.join(SITE_ROOT, '../userside/static')
+MEDIA_URL = os.path.join(SITE_ROOT, '../userside/static/')
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
--- timeinline/urls.py
+++ timeinline/urls.py
@@ -11,6 +11,9 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'userside.views.splash', name='splash'),
     url(r'^dashboard/(?P<caller_id>\d+)/$', 'userside.views.dashboard'),
+	url(r'^api/call/(?P<company_id>\d+)/(?P<caller_id>\d+)/$', 'userside.views.call_api'),
+	url(r'^api/hangup/(?P<company_id>\d+)/(?P<caller_id>\d+)/$', 'userside.views.hangup_api'),
+	url(r'^api/pickup/(?P<company_id>\d+)/(?P<caller_id>\d+)/$', 'userside.views.pickup_api'),
     # url(r'^timeinline/', include('timeinline.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
