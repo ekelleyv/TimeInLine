86174f7cd2e2eeca07d46c3618666a93649fc5cf
jurbanik <jurbanik@princeton.edu>
2012-03-31 20:21:19 -0400

    New dotcloud branch

--- timeinline/settings.py
+++ timeinline/settings.py
@@ -2,6 +2,11 @@
 
 import os
 import django
+
+import json
+with open('/home/dotcloud/environment.json') as f:
+  env = json.load(f)
+
 # calculated paths for django and the site
 # used as starting points for various other paths
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
@@ -19,12 +24,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'deployment.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME' : 'template1',
+        #'NAME': os.path.join(SITE_ROOT, 'deployment.db'),        # Or path to database file if using sqlite3.
+        'USER': env['DOTCLOUD_DB_SQL_LOGIN'],                      # Not used with sqlite3.
+        'PASSWORD': env['DOTCLOUD_DB_SQL_PASSWORD'],                  # Not used with sqlite3.
+        'HOST': env['DOTCLOUD_DB_SQL_HOST'],            # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': int(env['DOTCLOUD_DB_SQL_PORT']),        # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -56,18 +62,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/dotcloud/data/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = os.path.join(SITE_ROOT, '../userside/static/')
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/dotcloud/volatile/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -78,7 +84,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(SITE_ROOT, '../userside/static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -118,7 +123,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(SITE_ROOT, '../userside/static'),
 )
 
 INSTALLED_APPS = (
@@ -132,7 +136,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-	'django_evolution',
 	'userside',
 )
 
