f234536bb8f3acd5a483170b01806c2be58bea98
Craig Liebmann <craigliebmann@gmail.com>
2012-04-08 22:03:00 -0400

    revised stats

--- userside/stats.py
+++ userside/stats.py
@@ -69,10 +69,14 @@ def avg_wait_by_day(company):
     delta_avg_day[i] = deltasum_day[i] / count_day[i]
   return delta_avg_day
 
-# returns 7 lists of 24 timedeltas
-def avg_wait_by_day_hour(company):
+# returns tuple: (7 lists of 24 timedeltas/minutes, 7 lists of 24 counts)
+# excludes active calls and currently waiting
+def sum_wait_by_day_hour(company, retMinutes):
   calls = Call.objects.filter(company_id=company, callstart__isnull=False, callend__isnull=False)
-  deltasum_day_hour = [ [datetime.timedelta(0)*24] ]*7
+  if len(calls) == 0:
+    return None, None
+
+  sum_day_hour_td = [ [datetime.timedelta(0)]*24 ]*7
   count_day_hour = [ [0]*24 ]*7
   for call in calls:
     count_day_hour[call.callstart.day][call.callstart.hour] += 1
@@ -80,12 +84,66 @@ def avg_wait_by_day_hour(company):
       deltasum_day_hour[call.callstart.day][call.callstart.hour] += call.callend - call.callstart
     else:
       deltasum_day_hour[call.callstart.day][call.callstart.hour] += call.callanswered - call.callstart
-  delta_avg_day_hour = [ [datetime.timedelta(0)*24] ]*7
+ 
+  if not retMinutes:
+    return sum_day_hour_td, count_day_hour
+
+  avg_day_hour_m = [ [0]*24 ]*7
   for i in range(7):
     for j in range(24):
-      delta_avg_day_hour[i][j] = deltasum_day_hour[i][j] / count_day_hour[i][j]
-  return delta_avg_day_hour
+      avg_day_hour_m[i][j] = d.seconds / float(60) 
+  return avg_day_hour_m, count_day_hour
 
+# return 7 lists of 24 timedeltas/minutes
+def avg_wait_by_day_hour(company, retMinutes):
+  sums, counts = sum_wait_by_day_hour(company, retMinutes)
+  if sums is None:
+    return None
+  avg_day_hour = [ [0]*24 ]*7 if retMinutes else [ [datetime.timedelta(0)]*24 ]*7
+  for i in range(7):
+    for j in range(24):
+      avg_day_hour[i][j] = sums[i][j] / counts[i][j]
+  return avg_day_hour
 
+# return list (0-6) of timedeltas/minutes
+def avg_wait_by_day(company, retMinutes):
+  sums, counts = sum_wait_by_day_hour(company, retMinutes)
+  if sums is None:
+    return None
+  avg_day = [0]*7 if retMinutes else [datetime.timedelta(0)]*7
+  for i in range(7):
+    sum = 0 if retMinutes else datetime.timedelta(0)
+    count = 0
+    for j in range(24):
+      sum += sums[i][j]
+      count += counts[i][j]
+    avg_day[i] = sum / count
+  return avg_day
 
+# return list (0-7) of timedeltas/minutes
+def avg_wait_by_hour(company, retMinutes):
+  sums, counts = sum_wait_by_day_hour(company, retMinutes)
+  if sums is None:
+    return None
+  avg_hour = [0]*24 if retMinutes else [datetime.timedelta(0)]*24
+  for j in range(24):
+    sum = 0 if retMinutes else datetime.timedelta(0)
+    count = 0
+    for i in range(7):
+      sum += sums[i][j]
+      count += counts[i][j]
+    avg_hour[j] = sum / count
+  return avg_hour
 
+# returns timedelta/minutes
+def avg_wait(company, retMinutes):
+  sums, counts = sum_wait_by_day_hour(company, retMinutes)
+  if sums is None:
+    return None
+  sum = 0 if retMinutes else datetime.timedelta(0)
+  count = 0
+  for i in range(7):
+    for j in range(24):
+      sum += sums[i][j]
+      count += counts[i][j]
+  return sum / count
