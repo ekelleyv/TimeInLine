6a65d84349f6ed4b5dd884d330f5af53e197f129
jurbanik <jurbanik@princeton.edu>
2012-05-07 20:28:49 -0400

    Hope this works

--- timeinline/settings.py
+++ timeinline/settings.py
@@ -7,11 +7,8 @@ import json #for dot cloud
 # used as starting points for various other paths
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-#with open('/home/dotcloud/environment.json') as f:
-#	  env = json.load(f)
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+with open('/home/dotcloud/environment.json') as f:
+	  env = json.load(f)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,18 +58,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/dotcloud/data/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = os.path.join(SITE_ROOT, '../userside/static/')
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/dotcloud/volatile/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -80,12 +77,8 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-	os.path.join(SITE_ROOT, '../userside/static'),
+    #os.path.join(SITE_ROOT, 'static/'),
 )
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
